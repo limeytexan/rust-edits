0699a05340ec4c226ef3103f4c4f8a5904ff0480
etorreborre <etorreborre@yahoo.com>
2022-10-26 23:41:31 +0200

    project: fixed the README code
    with doctest

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! This crate provides a way to compare 2 strings and display their differences
+/// ```
+/// use edits::edits::*;
+///
+/// // "between the e and the n the letter i was added"
+/// assert_eq!(show_distance("kitten", "kittein"), "kitte[+i]n");
+///
+/// // "at the end of the text 3 letters have been deleted"
+/// assert_eq!(show_distance("kitten", "kit"), "kit[-t-e-n]");
+///
+/// // "between the t and the n 2 letters have been modified"
+/// assert_eq!(show_distance("kitten", "kitsin"), "kit[~t/s~e/i]n");
+///
+/// // "between the t and the n 2 letters have been modified"
+/// let my_split_size = SplitSize { split_size: 300 };
+/// assert_eq!(show_distance_with(my_split_size, default_display_options(), "kitten", "kitsin"), "kit[~t/s~e/i]n");
+/// ```
 pub mod color;
 pub mod costs;
 pub mod difference;
