5f509de6a6bd6654c71d5b2ac1267959e63631da
etorreborre <etorreborre@yahoo.com>
2022-10-26 23:41:30 +0200

    doc: added a bit more documentation

---
+++ README.md
@@ -0,0 +1,39 @@
+This library provides functions to show the differences between two strings.
+It uses the [Levenshtein distance](https://en.wikipedia.org/wiki/Levenshtein_distance) to compute
+the minimum number of edit operations: insertions, deletions, substitutions needed to go from one string to the other.
+
+Several options are available to customize this processing:
+
+  - `split_size` strings are broken into a list of lines if they contain any new lines first.
+     Then if any line is too large, according to the `split_size` it is broken into smaller pieces for comparison
+     This is done in order to reduce the size of the edit matrix which is used to compute all the edit costs
+     The default is 200
+
+  - `separators` opening and closing pieces of text (brackets by default) used to highlight a difference
+
+  - `shorten size` there is the possibly to display mostly the differences with a bit of context around if the input text is too large.
+      The string get elided around separators if it gets greater than the `shorten_size` (the default is 20)
+
+  - `shorten_text` the string to use when eliding characters in the original string (the default is `"..."`)
+
+  - `display_edit_operations` a function to specify how edit operations, insert/delete/substitute/keep are represented
+
+ Here are some examples:
+```rust
+use edits::*;
+
+// "between the e and the n the letter i was added"
+assert_equals!(show_distance("kitten", "kittein"), "kitte[+i]n");
+
+// "at the end of the text 3 letters have been deleted"
+assert_equals!(show_distance("kitten", "kit"), "kit[-t-e-n]");
+
+// "between the t and the n 2 letters have been modified"
+assert_equals!(show_distance("kitten", "kitsin"), "kit[~t/s~e/i]");
+
+// "between the t and the n 2 letters have been modified"
+let my_split_size = SplitSize { split_size = 300 };
+assert_equals!(show_distance_with(my_split_size, default_display_options, "kitten", "kitsin"), "kit[~t/s~e/i]");
+```
+The output can also be coloured. For example:
+<img src="doc/images/example.jpg" border="0"/>
--- src/edits.rs
+++ src/edits.rs
@@ -1,40 +1,37 @@
-//  This module provides a 'show_distance' function showing the differences between 2 pieces of text
-//   using the Levenshtein distance. That distance is defined as the minimum number of edits: insertions, deletions, substitutions
-//   to go from one text to another.
-//
-//   Several options are available to customize this processing:
-//
-//     - split size: texts are broken into new lines first. Then if the texts are too large there are split into smaller pieces
-//       in order to compute their difference. This is done in order to reduce the size of the edit matrix which is used to compute all the edit costs
-//       the default is 200
-//
-//     - separators: opening and closing pieces of text (brackets by default) used to highlight a difference
-//
-//     - shorten size: there is the possibly to display mostly the differences with a bit of context around if the input text is too large.
-//       The text gets elided around separators if it gets greater than the shorten size (the default is 20)
-//
-//     - shorten text: the text to use when eliding characters in the original text (the default is "...")
-//
-//     - display edit operations: edit operations, insert/delete/substitute/keep can be annotated if necessary
-//
-// Here are some examples:
-//
-// @
-// import Data.Text.Edits
-//
-// -- "between the e and the n the letter i was added"
-// showDistance "kitten" "kittein" === "kitte[+i]n"
-//
-// -- "at the end of the text 3 letters have been deleted"
-// showDistance "kitten" "kit" === "kit[-t-e-n]"
-//
-// -- "between the t and the n 2 letters have been modified"
-// showDistance "kitten" "kitsin" === "kit[~t/s~e/i]"
-// @
 use crate::costs::*;
 use crate::difference::*;
 use crate::edit_matrix::*;
 use crate::edit_operation::*;
+
+// Show the distance between 2 pieces of text
+pub fn show_distance<S1: Into<String>, S2: Into<String>>(s1: S1, s2: S2) -> String {
+    show_distance_with(default_split_size(), default_display_options(), s1, s2)
+}
+
+// Show the distance between 2 pieces of text with colors instead of symbols
+pub fn show_distance_colored<S1: Into<String>, S2: Into<String>>(s1: S1, s2: S2) -> String {
+    let mut options = default_display_options();
+    options.display_edit_operation = colored_display_edit_operation;
+    show_distance_with(default_split_size(), options, s1, s2)
+}
+
+// Show the distance between 2 pieces of text and specify splitting / display options
+pub fn show_distance_with<S1: Into<String>, S2: Into<String>>(
+    split_size: SplitSize,
+    display_options: DisplayOptions,
+    s1: S1,
+    s2: S2,
+) -> String {
+    let mut result: Vec<String> = vec![];
+    let splitted1 = split(split_size.clone(), s1.into());
+    let splitted2 = split(split_size, s2.into());
+    let zipped = splitted1.iter().zip(splitted2.iter());
+    for (line1, line2) in zipped {
+        let operations = levenshtein_operations(line1.clone(), line2.clone());
+        result.push(display_diffs(display_options.clone(), operations));
+    }
+    result.concat()
+}
 
 // Size to use when splitting a large piece of text
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -47,40 +44,10 @@ fn default_split_size() -> SplitSize {
     SplitSize { split_size: 200 }
 }
 
-// Show the distance between 2 pieces of text
-pub fn show_distance(s1: String, s2: String) -> String {
-    show_distance_with(default_split_size(), default_display_options(), s1, s2)
-}
-
-// Show the distance between 2 pieces of text with colors instead of symbols
-pub fn show_distance_colored(s1: String, s2: String) -> String {
-    let mut options = default_display_options();
-    options.display_edit_operation = colored_display_edit_operation;
-    show_distance_with(default_split_size(), options, s1, s2)
-}
-
-// Show the distance between 2 pieces of text and specify splitting / display options
-pub fn show_distance_with(
-    split_size: SplitSize,
-    display_options: DisplayOptions,
-    s1: String,
-    s2: String,
-) -> String {
-    let mut result: Vec<String> = vec![];
-    let splitted1 = split(split_size.clone(), s1);
-    let splitted2 = split(split_size, s2);
-    let zipped = splitted1.iter().zip(splitted2.iter());
-    for (line1, line2) in zipped {
-        let operations = levenshtein_operations(line1.clone(), line2.clone());
-        result.push(display_diffs(display_options.clone(), operations));
-    }
-    result.concat()
-}
-
 // Return the list of operations necessary to go from one piece of text to another
 // using the Levenshtein distance
-pub fn levenshtein_operations(s1: String, s2: String) -> Vec<EditOperation<char>> {
-    let matrix = create_edit_matrix(&levenshtein_costs(), s1.to_string(), s2.to_string());
+fn levenshtein_operations(s1: String, s2: String) -> Vec<EditOperation<char>> {
+    let matrix = create_edit_matrix(&levenshtein_costs(), s1.clone(), s2.clone());
     make_edit_operations(s1.chars().collect(), s2.chars().collect(), matrix)
 }
 
@@ -136,30 +103,30 @@ mod tests {
     }
     #[test]
     fn test_show_distance() {
-        assert_eq!(show_distance("k".to_string(), "l".to_string()), "[~k/l]");
-        assert_eq!(show_distance("ki".to_string(), "ka".to_string()), "k[~i/a]");
+        assert_eq!(show_distance("k", "l"), "[~k/l]");
+        assert_eq!(show_distance("ki", "ka"), "k[~i/a]");
         assert_eq!(
-            show_distance("kitte".to_string(), "kittei".to_string()),
+            show_distance("kitte", "kittei"),
             "kitte[+i]"
         );
         assert_eq!(
-            show_distance("kitten".to_string(), "kittein".to_string()),
+            show_distance("kitten", "kittein"),
             "kitte[+i]n"
         );
         assert_eq!(
-            show_distance("kitten".to_string(), "kit".to_string()),
+            show_distance("kitten", "kit"),
             "kit[-t-e-n]"
         );
         assert_eq!(
-            show_distance("kit".to_string(), "kitten".to_string()),
+            show_distance("kit", "kitten"),
             "kit[+t+e+n]"
         );
         assert_eq!(
-            show_distance("kitten".to_string(), "kitsin".to_string()),
+            show_distance("kitten", "kitsin"),
             "kit[~t/s~e/i]n"
         );
         assert_eq!(
-            show_distance("kitte".to_string(), "kitte".to_string()),
+            show_distance("kitte", "kitte"),
             "kitte"
         );
     }
