66b14a39623128058660e8959e187c574627ed92
etorreborre <etorreborre@yahoo.com>
2022-10-30 16:20:14 +0100

    project: use cargo-tarpaulin only on Darwin

--- pkgs/edits/default.nix
+++ pkgs/edits/default.nix
@@ -30,7 +30,7 @@ rustPlatform.buildRustPackage rec {
 
 
 
-  # Non-Rust runtime dependencies (most likely libraries) of your project can 
+  # Non-Rust runtime dependencies (most likely libraries) of your project can
   # be added in buildInputs.
   # Make sure to import any additional dependencies above
   buildInputs =
@@ -60,8 +60,7 @@ rustPlatform.buildRustPackage rec {
   nativeBuildInputs = [
     pkg-config # for openssl
     evcxr
-    cargo-tarpaulin
-  ];
+  ] ++ lib.optional hostPlatform.isDarwin [ cargo-tarpaulin ];
 
   meta = with lib; {
     description = "This library displays the difference between 2 strings using the Levenshtein distance";
